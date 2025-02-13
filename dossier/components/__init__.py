b76e5bb0a538dcc872fb003999967098c945fb80
BerlitoPyth <berliatadrien@gmail.com>
2025-02-13 15:04:33 +0100

    Update __init__.py

--- dossier/components/__init__.py
+++ dossier/components/__init__.py
@@ -1,3 +1,13 @@
 from .quiz import display_quiz
+from .presentation import display_presentation
+from .floating_chat import add_floating_chat_to_app
+from .projet_gaming import display_project_concept
+from .navigation import display_navigation
 
-__all__ = ['display_quiz']
+__all__ = [
+    'display_quiz',
+    'display_presentation',
+    'add_floating_chat_to_app',
+    'display_project_concept',
+    'display_navigation'
+]
